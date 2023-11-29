8cde230f644d059160e346275c8c81700742d834
sasijonnada72 <150852967+sasijonnada72@users.noreply.github.com>
2023-11-28 23:47:56 -0500

    first commit

---
+++ app/src/main/java/com/example/personalhealthtracker/data/HealthyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.personalhealthtracker.data
+
+
+class HealthyActivity (var activityName:String?, var timeElapsed:String?,
+                       var roadTravelled:String?,
+                       var energyCons: String?,
+                       var imageUrl: String?
+                        ){
+
+
+}
---
+++ app/src/main/java/com/example/personalhealthtracker/other/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.personalhealthtracker.other
+
+import android.annotation.SuppressLint
+import com.google.firebase.database.collection.LLRBNode.Color
+
+@SuppressLint("RestrictedApi")
+object Constants {
+    const val PERMISSION_LOCATION_REQUEST_CODE = 1
+
+}
